e75483ce72522d5c2a8457f1e49a57eb298a8157
Сергей Дьяконов <SirDknv@gmail.com>
2024-10-05 05:36:29 +0300

    First commit

---
+++ time.pas
@@ -0,0 +1,9 @@
+﻿##
+var n := readinteger('Введите год:');
+
+if (n mod 4 = 0) then
+  if n mod 100 = 0 then
+    if n mod 400 = 0 then print('Этот год високосный')
+    else print('Этот год не високосный')
+  else print('Этот год високосный')
+else print('Этот год не високосный')
