cc98e33e9558329631514e5f7735072798839c25
Сергей Дьяконов <SirDknv@gmail.com>
2024-10-05 05:40:01 +0300

    First commit

---
+++ timer.pas
@@ -0,0 +1,9 @@
+﻿##
+var n := readinteger('Введите год:');
+
+if (n mod 4 = 0) then
+  if n mod 100 = 0 then
+    if n mod 400 = 0 then print('В этом году 366 дней')
+    else print('В этом году 365 дней')
+  else print('В этом году 366 дней')
+else print('в этом году 365 дней')
